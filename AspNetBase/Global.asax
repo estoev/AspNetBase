e54ee16088bb97bbc4bacb0b8816684747ecd204
Emil Stoev <emil@estreambg.com>
2015-10-06 11:20:04 +0300

    Starting from the Visual Studio web site project template

---
+++ AspNetBase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetBase.MvcApplication" Language="C#" %>
---
+++ AspNetBase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetBase.Startup))]
+namespace AspNetBase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
